a1d9ad5e0e6a4dad137b3d65746bcfa4e0ad91fb
Arya0612-web <aryaabdilah67@gmail.com>
2026-02-06 10:52:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -42,15 +42,16 @@ app.use('/uploads', express.static(uploadsDir, {
 
 // Database connection
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 };
+
 
 const pool = mysql.createPool(dbConfig);
 
